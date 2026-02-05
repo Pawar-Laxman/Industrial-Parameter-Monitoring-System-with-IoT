1a36a3b1fb354f2a836348da36c1476f783519ec
Pawar-Laxman <pawarpottoda@gmail.com>
2026-02-05 14:30:20 +0530

    Add files via upload

---
+++ industrial_iot_alarm.ino
@@ -0,0 +1,33 @@
+#include <ESP8266WiFi.h>
+
+// -------- WIFI DETAILS ----------
+const char* ssid = "YOUR_WIFI_NAME";
+const char* password = "YOUR_WIFI_PASSWORD";
+
+// -------- PIN ----------
+#define BUZZER D5
+
+void setup() {
+  pinMode(BUZZER, OUTPUT);
+  digitalWrite(BUZZER, LOW);
+
+  Serial.begin(9600);
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("WiFi Connected");
+}
+
+void loop() {
+  // Example alarm condition
+  digitalWrite(BUZZER, HIGH);
+  delay(2000);
+
+  digitalWrite(BUZZER, LOW);
+  delay(2000);
+}
